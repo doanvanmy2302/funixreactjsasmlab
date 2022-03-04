46ff47213b85ae1212b6fbd783e77620d813e23e
My <my.dv2002@gmail.com>
2022-03-04 07:59:01 +0700

    install dateformat and update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-
+import React, { Component } from 'react';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import StaffList from './StaffList/StaffListComponent';
+import { STAFFS } from './shared/staffs';
+import { DEPARTMENTS } from './shared/staffs';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs: STAFFS,
+      department: DEPARTMENTS
+    };
+  }
+  render() {
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+        <StaffList staffs={this.state.staffs} />
+      </div>
+    );
+  }
 }
 
 export default App;
