d5ff3c64e87e5b13544b1642fa11a76de9c35a3f
My <my.dv2002@gmail.com>
2022-03-04 11:12:58 +0700

    Presentational and Container Components

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+  Card, CardImg, CardImgOverlay,
+  CardTitle
+} from 'reactstrap';
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
+
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-1 ">
+          <Card
+            onClick={() => this.props.onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+
+          </Card>
+        </div>
+      );
+    });
+
+    return (
+      <div className="container">
+        <div className="row">
+          {menu}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
