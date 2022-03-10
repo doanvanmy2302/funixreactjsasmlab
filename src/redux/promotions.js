497c5f9d5aa0fc4b2a7b9c3c30da9055de074b53
My <my.dv2002@gmail.com>
2022-03-10 08:51:17 +0700

    react animations

---
+++ src/redux/promotions.js
@@ -0,0 +1,18 @@
+import * as ActionTypes  from './ActionTypes';
+
+export const Promotions = (state = {
+    isLoading:true, errMess:null, promos:[]
+}, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_PROMOS:
+                return {...state, isLoading: false, errMess: null, promos: action.payload};
+    
+            case ActionTypes.PROMOS_LOADING:
+                return {...state, isLoading: true, errMess: null}
+    
+            case ActionTypes.PROMOS_FAILED:
+                return {...state, isLoading: false, errMess: action.payload};
+            default:
+            return state       
+      }
+};
